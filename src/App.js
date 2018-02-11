5719276ca0484791ca84fcc53577ce40adaf8746
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2018-02-11 13:30:09 -0500

    Add SCSS watcher

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import SoundGrid from './SoundGrid';
 import Tone from 'tone';
+
 class App extends Component {
   render() {
-
     return (
-      <div className="App">
-
+      <div className="wrapper">
+        <SoundGrid />
       </div>
     );
   }
 
   componentDidMount() {
-    var synth = new Tone.Synth().toMaster();
-    synth.triggerAttackRelease("D#2", "8n");
+    // var synth = new Tone.Synth().toMaster();
+    // synth.triggerAttackRelease("D#2", "16n");
   }
 }
 
---
+++ src/SoundGrid/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SoundGrid extends Component {
+  render() {
+    return (
+      <div className="sound-grid">
+        <h1>SoundGrid</h1>
+      </div>
+    );
+  }
+}
+
+export default SoundGrid;
